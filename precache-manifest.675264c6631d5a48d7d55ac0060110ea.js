70c32fcd0906267a8e44e41be3f4aae6839131cf
Nikita Pavlov <npavlov@outlook.com>
2019-09-17 14:26:12 +0300

    Updates

---
+++ precache-manifest.675264c6631d5a48d7d55ac0060110ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e7ea501e7b8f22ce0b7",
+    "url": "/weatherapp/static/js/runtime~main.0e7ea501.js"
+  },
+  {
+    "revision": "bbccac64918907a595ad",
+    "url": "/weatherapp/static/js/main.bbccac64.chunk.js"
+  },
+  {
+    "revision": "fa75babff0d694b4ec05",
+    "url": "/weatherapp/static/js/1.fa75babf.chunk.js"
+  },
+  {
+    "revision": "bbccac64918907a595ad",
+    "url": "/weatherapp/static/css/main.6b3272c8.chunk.css"
+  },
+  {
+    "revision": "d069fd40138e3325796f62e8023abac1",
+    "url": "/weatherapp/index.html"
+  }
+];
